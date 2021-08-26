a7d6468eed76b893c338e714a166b152adc93b99
aghArdeshir <ardeshireo@gmail.com>
2021-08-27 01:13:21 +0400

    make title click scroll to top

--- src/Title.tsx
+++ src/Title.tsx
@@ -2,8 +2,15 @@ import styled from "styled-components";
 
 const Text = styled.h1`
   font-size: 14px;
+
+  user-select: none;
+  cursor: pointer;
 `;
 
+function scrollToTop() {
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+}
+
 export default function Title() {
-  return <Text>Where in the world?</Text>;
+  return <Text onClick={scrollToTop}>Where in the world?</Text>;
 }
